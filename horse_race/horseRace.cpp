eb928b0b840a61a3f2154bc288cd1b762870c657
Casito288 <ethancastro0805@proton.me>
2026-09-16 16:49:37 +0000

    starting to code

--- horse_race/horseRace.cpp
+++ horse_race/horseRace.cpp
@@ -4,11 +4,22 @@ void advance(int horseNum, int* horses);
 void printlIne(int horseNum, int* horses);
 void isWinner(int horseNum, int* horses);
 
-const MAX = 15;
+const MAX_LENGTH = 15;
+const MAX_HORSES = 5;
+
+std::random_device rd;
+std::uniform_int_distribution<int> dist(0, 1);
 
 int main (){
 
   int horseName[] {0, 1, 2, 3, 4};
-  boolean keepGoing
+  boolean keepGoing = true;
+  
+  while (keepGoing){
+  
+    std::cout << "Press enter to begin the reace" << endl:
+    
+
+  }
   
 }
