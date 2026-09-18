5f24c915a2224778ff7b7b2fd4a041245f6a708e
Casito288 <ethancastro0805@proton.me>
2026-09-18 01:05:46 +0000

    Finished most of the project, just need to implement the isWinner function

--- horse_race/horseRace.cpp
+++ horse_race/horseRace.cpp
@@ -1,25 +1,67 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
 
-void advance(int horseNum, int* horses);
-void printlIne(int horseNum, int* horses);
-void isWinner(int horseNum, int* horses);
+void advance(int* horses);
+void printLane(int* horses);
+//bool isWinner(int horseNum, int* horses);
 
-const MAX_LENGTH = 15;
-const MAX_HORSES = 5;
+const int MAX_LENGTH = 15;
+const int MAX_HORSES = 5;
 
-std::random_device rd;
-std::uniform_int_distribution<int> dist(0, 1);
 
 int main (){
 
-  int horseName[] {0, 1, 2, 3, 4};
-  boolean keepGoing = true;
-  
-  while (keepGoing){
-  
-    std::cout << "Press enter to begin the reace" << endl:
-    
+  int horses[MAX_HORSES] = {0, 0, 0, 0, 0};
+  // int horseNum = 0;
+  bool keepGoing = true;
+  srand(time(NULL));
 
-  }
+  std::cout << "Would you like to play a game? Press enter to begin: " << std::endl;
+  std::cin.get();
+
+  while (keepGoing){
+
+    advance(horses);
+    printLane(horses);
+    
+    for (int i = 0; i < MAX_LENGTH; i++) {
+      if (horse[i] == MAX_LENGTH - 1) {
+        keepGoing = false;
+      } else {
+        std::cout << "press enter to flip the coin again" << std::endl;
+        std::cin.get();
+
+      } // end if statement
+    } // end for loop
+  } // end while loop
+} // end main function
+
+
+void advance(int* horses){
   
-}
+  for (int i = 0; i < MAX_HORSES; i++){
+    int rd = rand() % 2;
+    if (rd == 1){
+      horses[i] += 1;
+
+    } // end if statement
+  } // end for loop
+} // end advance function
+
+void printLane(int* horses){
+
+  std::string horseID[] = {"Lenard", "Klein", "Alger", "Audrey", "Derrick", "Emyln"};
+  char horseLetter[] = {'L', 'K', 'A', 'U', 'D'};
+  
+  for (int i = 0; i < MAX_HORSES; i++){
+    std::string lane(MAX_LENGTH, '-');
+
+    if (horses[i] >= 0 && horses[i] < MAX_LENGTH) {
+      lane[horses[i]] = horseLetter[i];
+      std::cout << "Horse: " << horseID[i] << " |" << lane << "|" << std::endl;
+
+    } // end if statement
+  } // end for loop
+} // end printLane function
